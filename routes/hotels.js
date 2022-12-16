545d7bb49c0de8ba62773f3bdf5beed167c2c539
demetgas <demetgas@github.com>
2022-12-16 23:56:02 +0300

    Update hotels.js
    adding post delete put get

--- routes/hotels.js
+++ routes/hotels.js
@@ -10,18 +10,72 @@ router.post("/", async (req, res) => {
   const newHotel = new Hotel(req.body);
   try {
     const savedHotel = await newHotel.save();
+    //if its succesfull we will create a new hotel
     res.status(200).json(savedHotel);
+    //if not we will have an error
   } catch (e) {
     res.status(500).json(e);
     //500 is a server error, here if an error occurs we will handle and specify them and we will handle them using express middlewares
   }
 });
 // deleting existing hotels
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    //if its succesfull we will delete the hotel
+    res.status(200).json("You just deleted the hotel.");
+    //if not we will have an error
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
 
 //updating existing hotels
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body } /*updates*/,
+      { new: true } /*returns the updated version */
+    );
 
-//getting
+    //if its succesfull we will get the updated hotel
 
-//getting all
+    res.status(200).json(updatedHotel);
+
+    //if not we will have an error
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+//getting a specific hotel
+
+router.get("/:id", async (req, res) => {
+  try {
+    await hotel.findById(req.params.id);
+
+    //if its succesfull we will get the hotels
+    res.status(200).json(hotel);
+
+    //if not we will have an error
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+//getting all the hotels that exist
+
+router.get("/:id", async (req, res) => {
+  try {
+    await hotel.findById(req.params.id);
+
+    //if its succesfull we will get the hotel
+    res.status(200).json(hotel);
+
+    //if not we will have an error
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
 
 export default router;
