837cfb6cd2d8ccf301b95bf552831402e9fa7dfb
demetgas <demetgas@github.com>
2022-12-19 16:06:05 +0300

    fixing login function and adding a comment in register function about bcryptjs library

--- utils/controllers/confirmController.js
+++ utils/controllers/confirmController.js
@@ -3,6 +3,8 @@ import bcrypt from "bcryptjs";
 
 export const register = async (req, res, next) => {
   try {
+
+    // adding hash with bcryptjs library so our password wont be seen by everyone in mongodb instead it will show a hash number
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.pwd, salt);
     const newUser = new User({
@@ -19,14 +21,8 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
   try {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.pwd, salt);
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      pwd: hash,
-    });
-    await newUser.save();
+    const user = User.find({username:req.body.username})
+    if(!user) return next(createError(404,"User not found"))
     res.status(201).send("user successfully registered");
   } catch (e) {
     next(e);
