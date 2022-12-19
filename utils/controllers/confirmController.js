35639acb7f10b7d3c2ae8bfffdf0fe7c0632becf
demetgas <demetgas@github.com>
2022-12-19 20:07:17 +0300

    formatted the code

--- utils/controllers/confirmController.js
+++ utils/controllers/confirmController.js
@@ -1,10 +1,9 @@
 import User from "../../modelsof/User.js";
 import bcrypt from "bcryptjs";
-import {createError} from "../../utils/error.js"
+import { createError } from "../../utils/error.js";
 
 export const register = async (req, res, next) => {
   try {
-
     // adding hash with bcryptjs library so our password wont be seen by everyone in mongodb instead it will show a hash number
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.pwd, salt);
@@ -22,13 +21,14 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
   try {
-    const user = await User.findOne({username:req.body.username})
-    if(!user) return next(createError(404,"User not found"))
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found"));
 
-    const isPwdCorrect = await bcrypt.compare(req.body.pwd, user.pwd)
-    if(!isPwdCorrect) return next(createError(400,"Wrong password or username"))
-    // pwd and isAdmin features won't be shown 
-    const {pwd,isAdmin, ...otherDetails}=user._doc;
+    const isPwdCorrect = await bcrypt.compare(req.body.pwd, user.pwd);
+    if (!isPwdCorrect)
+      return next(createError(400, "Wrong password or username"));
+    // pwd and isAdmin features won't be shown
+    const { pwd, isAdmin, ...otherDetails } = user._doc;
     res.status(201).json(otherDetails);
   } catch (e) {
     next(e);
