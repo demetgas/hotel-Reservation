f3fd84c1796d90d64c77d47b81d85fc42aa7064e
demetgas <demetgas@github.com>
2023-03-15 15:51:17 +0100

    verifyToken updated

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,2 +1,12 @@
 import  jwt  from "jsonwebtoken";
 import { createError } from "./error.js";
+
+export const verifyToken = (req,res,next)=>{
+    const token = req.cookies.access_token;
+    if(!token){
+        return next(createError(401,"Authentication failed!"))
+    }
+    jwt.verify(token,process.env.JWT,(err,user)=>(
+        
+    ))
+}
