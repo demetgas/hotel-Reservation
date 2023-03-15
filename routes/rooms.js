3702d2628a6276c56b8615405f5701f512ff3275
demetgas <demetgas@github.com>
2023-03-15 19:19:36 +0100

    Update rooms.js

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,6 +1,21 @@
 import express from "express";
+import {verifyAdmin} from "../utils/verifyToken"
 
 const router = express.Router();
 
+// creating  new hotels
+router.post("/", verifyAdmin, createRoom);
+
+// deleting existing hotels
+router.delete("/:id", verifyAdmin, deleteHotel);
+
+// updating existing hotels
+router.put("/:id", verifyAdmin, updateHotel);
+
+// getting a specific hotel
+router.get("/:id", getHotel);
+
+// getting all the hotels that exist
+router.get("/", getAllHotels);
 
 export default router
