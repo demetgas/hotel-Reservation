c29f9343932e942b1b5a8dc28f96bc7dc4d4780b
demetgas <demetgas@github.com>
2023-03-15 18:27:01 +0100

    fixed room model

--- modelsof/Room.js
+++ modelsof/Room.js
@@ -1,24 +1,26 @@
 import mongoose from "mongoose";
 
-const RoomSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: false
+const RoomSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    maxPeople: {
+      type: Number,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    roomNumbers: [{}],
   },
-  email: {
-    type: String,
-    required: false,
-    unique: true,
-  },
-  pwd: {
-    type: String,
-    required: false,
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-    required: false,
-  },
-},{timestamps:true});
+  { timestamps: true }
+);
 
 export default mongoose.model("Room", RoomSchema);
