dacf33a3970bb99b579e340c861dec9d651665e9
demetgas <demet_gas@hotmail.com>
2023-05-22 21:55:52 +0200

    addet limit to query

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -60,7 +60,7 @@ export const getHotel = async (req, res, next) => {
 //Getting all the hotels that exist
 export const getAllHotels = async (req, res, next) => {
   try {
-    const hotels = await Hotel.find(req.query);
+    const hotels = await Hotel.find(req.query).limit(req.query.limit);
 
     //if its succesfull we will get the hotel
     res.status(200).json(hotels);
