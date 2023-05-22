86185c076fbe34d161c7dcb550fae4ca672692f6
demetgas <demet_gas@hotmail.com>
2023-05-22 20:03:08 +0200

    countbytype imported

--- routes/hotels.js
+++ routes/hotels.js
@@ -2,6 +2,7 @@ import express from "express";
 import Hotel from "../modelsof/Hotel.js";
 import {
   countByCity,
+  countByType,
   createHotel,
   deleteHotel,
   getAllHotels,
@@ -28,6 +29,6 @@ router.get("/find/:id", getHotel);
 // getting all the hotels that exist
 router.get("/", getAllHotels);
 router.get("/countByCity", countByCity);
-router.get("/countByType", getAllHotels);
+router.get("/countByType", countByType);
 
 export default router;
