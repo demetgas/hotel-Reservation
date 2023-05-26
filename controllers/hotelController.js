aa6e915f1e386c796647b99d83a266b6c4e35349
demetgas <demet_gas@hotmail.com>
2023-05-26 22:54:53 +0200

    error fixed

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -113,7 +113,7 @@ export const countByType = async (req, res, next) => {
 
 export const getRooms = async (req, res, next) => {
   try {
-    const hotel = Hotel.findById(req.params.id);
+    const hotel = await Hotel.findById(req.params.id);
     const list = await Promise.all(
       hotel.rooms.map((room) => {
         return Room.findById(room);
